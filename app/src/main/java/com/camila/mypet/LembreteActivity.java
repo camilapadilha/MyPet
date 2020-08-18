7fdef65294b56aab2d2099d316a23fb79866bb81
Camila Padilha <camilapadilha1997@gmail.com>
2020-08-18 00:35:37 -0300

    primeiro commit

---
+++ app/src/main/java/com/camila/mypet/LembreteActivity.java
@@ -0,0 +1,14 @@
+package com.camila.mypet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LembreteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lembrete);
+    }
+}
---
+++ app/src/main/java/com/camila/mypet/VerPetActivity.java
@@ -0,0 +1,14 @@
+package com.camila.mypet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_pet);
+    }
+}
